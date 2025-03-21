c60dd25882191632479d193112dbb7110ad7b925
AntonioManuel2006 <agarlun2008@g.educaand.es>
2025-03-21 13:03:47 +0000

    ...............

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,1 +1,24 @@
-//Escribe aquí tu código JavaScript
+//Validacion del formulario
+
+
+
+
+
+
+
+//Confirmacion antes de enviar el pedido
+document.getElementById("Enviar").onclick = function EnviarPedido() {
+    let respuesta = confirm("¿Estas seguro de enviar el pedido?");
+    if (respuesta == true) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+//modo claro y oscuro
+document.getElementById("modoBTN").onclick = function(){
+    document.body.classList.toggle("oscuro");
+}
+
+
