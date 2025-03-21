f9e6fa94a68d366ec51834d759bcdaaf7e968393
AntonioManuel2006 <agarlun2008@g.educaand.es>
2025-03-21 13:22:20 +0000

    .......................

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,4 +1,31 @@
 //Validacion del formulario
+document.getElementById("formulario").onsubmit = function EnviarPedido() {
+
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let telefono = document.getElementById("telefono").value;
+    let direccion = document.getElementById("direccion").value;
+
+ 
+    if (!/^[a-zA-Z\s]+$/.test(nombre)) {
+        alert("El nombre solo puede contener letras y espacios.");
+        return false;
+    }
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        alert("Por favor, introduce un correo electrónico válido.");
+        return false;
+    }
+
+    if (!/^\d{9}$/.test(telefono)) {
+        alert("El teléfono debe contener exactamente 9 números.");
+        return false;
+    }
+
+    if (direccion.length < 18) {
+        alert("La dirección debe tener al menos 5 caracteres.");
+        return false;
+    }
 
 
 
@@ -21,4 +48,24 @@ document.getElementById("modoBTN").onclick = function(){
     document.body.classList.toggle("oscuro");
 }
 
+//Contador de caracteres limitado a 150
+document.getElementById("adicional").oninput = function(){
+    let caracteres = this.value.length;
+    document.getElementById("contador").innerHTML = caracteres;
+    this.disabled = caracteres > 150;
+}
 
+
+
+//Cambiar color del input type submit al pasar el mouse
+let enviarButton = document.getElementById("Enviar");
+
+enviarButton.addEventListener("mouseover", () => {
+    enviarButton.style.backgroundColor = "black";
+    enviarButton.style.color = "white";
+});
+
+enviarButton.addEventListener("mouseout", () => {
+    enviarButton.style.backgroundColor = "white";
+    enviarButton.style.color = "black";
+}); 
